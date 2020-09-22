f450797df11d84c16e3aac71acc14cf2a991e3ed
Avreghly Barra <avreghly@gmail.com>
2020-09-22 17:57:57 +0700

    define handlers

--- server/handlers.js
+++ server/handlers.js
@@ -1,6 +1,42 @@
+const kontakstore = require('../services/kontak-store')
+
 module.exports = {
-    InsertContactV1: async function (req, res) { },
-    GetContactV1: async function (req, res) { },
-    UpdateContactV1: async function (req, res) { },
-    DeleteContactV1: async function (req, res) { },
+    InsertContactV1: async function (req, res) {
+        const id = ''
+        const kontak = await kontakstore.create({
+            id: id,
+            name: req.body.name,
+            nomor: req.body.telepon,
+        })
+
+        res.json(kontak)
+    },
+
+    GetContactAllV1: async function (req, res) {
+        const results = await kontakstore.find({})
+        res.json(results)
+    },
+
+    GetContactV1: async function (req, res) {
+        const result = await kontakstore.findOne({ id: req.params.id })
+        res.json(result)
+    },
+
+    UpdateContactV1: async function (req, res) {
+        const kontak = await kontakstore.findOne({ id: req.params.id })
+        const newkontak = await kontak.update({
+            name: req.body.name,
+            nomor: req.body.telepon,
+        })
+
+        res.json(newkontak)
+    },
+
+    DeleteContactV1: async function (req, res) {
+        const kontak = await kontakstore.findOne({ id: req.params.id })
+        const newkontak = await kontak.remove()
+
+        res.json(newkontak)
+    },
+
 }
